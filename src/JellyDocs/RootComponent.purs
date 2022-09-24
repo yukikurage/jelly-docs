7da73bed294d48243cc1554ecc1778609373493d
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-09-25 01:06:55 +0900

    fix css loading

--- src/JellyDocs/RootComponent.purs
+++ src/JellyDocs/RootComponent.purs
@@ -49,24 +49,20 @@ rootComponent = hooks do
           ]
           mempty
         el "link"
-          [ "rel" := "preload"
-          , "as" := "style"
-          , "href" := "https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Montserrat:wght@700&family=Source+Code+Pro&display=swap"
-          ]
-          mempty
-        el "link"
           [ "rel" := "stylesheet"
           , "href" := "https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Montserrat:wght@700&family=Source+Code+Pro&display=swap"
-          , "media" := "print"
-          , "onload" := "this.media='all'"
           ]
           mempty
 
         el "link"
+          [ "rel" := "preconnect"
+          , "href" := "https://cdnjs.cloudflare.com"
+          , "crossorigin" := true
+          ]
+          mempty
+        el "link"
           [ "rel" := "stylesheet"
           , "href" := "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.10/styles/atom-one-light.min.css"
-          , "media" := "print"
-          , "onload" := "this.media='all'"
           ]
           mempty
 
