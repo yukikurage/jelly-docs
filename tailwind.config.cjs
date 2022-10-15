754efe69de55eb4aa2702e1db524fd42a58fc7a0
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-10-16 01:13:13 +0900

    fix tai;wind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,8 +8,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        Lato: ['Lato', 'Noto Color Emoji', 'sans-serif'],
-        Montserrat: ['Montserrat', 'Noto Color Emoji', 'sans-serif'],
+        Lato: ['Lato', 'sans-serif'],
+        Montserrat: ['Montserrat', 'sans-serif'],
         SourceCodePro: ['Source Code Pro', 'monospace'],
       }
     },
