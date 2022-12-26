e9180fda5cecafe74ad77d559ef442edea2f1c7b
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-12-27 00:02:51 +0900

    fix twemoji URL

--- src/JellyDocs/Twemoji.js
+++ src/JellyDocs/Twemoji.js
@@ -1,3 +1,5 @@
 import twemoji from "twemoji";
 
-export const parseEmoji = (el) => () => {twemoji.parse(el, {folder: "svg", ext: ".svg"})};
+export const parseEmoji = (el) => () => {
+  twemoji.parse(el, { folder: "svg", ext: ".svg", base: 'https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/' });
+};
